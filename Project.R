79f562278565a6b408e160f3716ea239127f4780
Casey Burhanna <clb168@case.edu>
2023-12-06 20:28:05 -0500

    monthly averages

--- Project.R
+++ Project.R
@@ -3,7 +3,7 @@ library(plotly)
 library(ggplot2)
 library(TTR)
 # Read CSV file
-my_data <- read.csv("/Users/cburh/Documents/Assignments_Fall2023/Stat_332/Project/SBUX_1yr.csv")
+my_data <- read.csv("/Users/cburh/Documents/Assignments_Fall2023/Stat_332/Project/SBUX.csv")
 
 my_data[,1] <- as.Date(my_data[,1], format="%m/%d/%Y")
 
@@ -19,11 +19,58 @@ ggplot(my_dataframe, aes(x = Time, y = Value)) +
 
 
 # decomposition
-decomp_data = SMA(my_dataframe, n=10)
+decomp_data = SMA(my_dataframe, n=30)
 plot.ts(decomp_data)
-print(decomp_data)
+#print(decomp_data)
+
+#TESTING SEASONALIty
+#d = fft(my_dataframe)
+#decomposed_data=decompose(d,type='multiplicative')
+#autoplot(fft(decomposed_data))
 
 #partial auto correlation
 acf(my_dataframe, type = c("partial"), plot = TRUE)
 
+#mean squared error
 
+
+#ARIMA plot
+
+
+#montly averages
+my_data <- read.csv("/Users/cburh/Documents/Assignments_Fall2023/Stat_332/Project/SBUX.csv")
+Open_ts = ts(my_data[,1], frequency = 1)
+
+monthly_avg = array(0, dim = c(500))
+month=month(mdy(my_data[,1]))
+# Extract the 4th column
+column_data <- my_data[, 4]
+
+# Remove the dollar sign and convert to numeric
+numeric_data <- as.numeric(sub("\\$", "", column_data))
+#print(month)
+temp = 5
+j=1
+month_iter = 1
+for (i in 1:length(month)){
+  print(i)
+  if (i==1){
+    monthly_avg[j] = numeric_data[1]
+    print(monthly_avg[j])
+    
+  }
+  else if (month[i] == month[i-1]){
+    monthly_avg[j] = monthly_avg[j] + numeric_data[i]
+    print(monthly_avg[j])
+    month_iter = month_iter + 1
+  }
+  else if (month[i] != month[i-1]) {
+    monthly_avg[j] = monthly_avg[j]/month_iter
+    month_iter = 1
+    j = j+1
+    print(j)
+    monthly_avg[j] = numeric_data[i]
+  }
+}
+
+print(monthly_avg)
