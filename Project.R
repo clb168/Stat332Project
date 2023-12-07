1b77b41ee9cb3a932ff15bb099a391f0149dab7e
Casey Burhanna <clb168@case.edu>
2023-12-06 21:49:48 -0500

    commit

--- Project.R
+++ Project.R
@@ -3,24 +3,24 @@ library(plotly)
 library(ggplot2)
 library(TTR)
 # Read CSV file
-my_data <- read.csv("/Users/cburh/Documents/Assignments_Fall2023/Stat_332/Project/SBUX.csv")
+#my_data <- read.csv("/Users/cburh/Documents/Assignments_Fall2023/Stat_332/Project/SBUX.csv")
 
-my_data[,1] <- as.Date(my_data[,1], format="%m/%d/%Y")
+#my_data[,1] <- as.Date(my_data[,1], format="%m/%d/%Y")
 
-my_dataframe <- data.frame(Time = my_data[,1], Value = my_data[,4])
+#my_dataframe <- data.frame(Time = my_data[,1], Value = my_data[,4])
 
-my_dataframe$Value <- as.numeric(gsub("[\\$,]", "", my_dataframe$Value))
+#my_dataframe$Value <- as.numeric(gsub("[\\$,]", "", my_dataframe$Value))
 
 # time series plot 
-ggplot(my_dataframe, aes(x = Time, y = Value)) +
-  geom_line() +  # This adds a line plot
-  xlab("\n Date") +
-  ylab("Price in dollars \n")
+#ggplot(my_dataframe, aes(x = Time, y = Value)) +
+  #geom_line() +  # This adds a line plot
+  #xlab("\n Date") +
+  #ylab("Price in dollars \n")
 
 
 # decomposition
-decomp_data = SMA(my_dataframe, n=30)
-plot.ts(decomp_data)
+#decomp_data = SMA(my_dataframe, n=30)
+#plot.ts(decomp_data)
 #print(decomp_data)
 
 #TESTING SEASONALIty
@@ -29,7 +29,7 @@ plot.ts(decomp_data)
 #autoplot(fft(decomposed_data))
 
 #partial auto correlation
-acf(my_dataframe, type = c("partial"), plot = TRUE)
+#acf(my_dataframe, type = c("partial"), plot = TRUE)
 
 #mean squared error
 
@@ -48,24 +48,29 @@ column_data <- my_data[, 4]
 
 month_year = array(" ", dim = c(120))
 year=year(mdy(my_data[,1]))
-
+#month_year[120] = "12/2013"
 # Remove the dollar sign and convert to numeric
 numeric_data <- as.numeric(sub("\\$", "", column_data))
 #print(month)
 temp = 5
 j=1
+
 month_iter = 1
 for (i in 1:length(month)){
-  print(i)
+  #print(i)
   if (i==1){
     monthly_avg[j] = numeric_data[1]
-    print(monthly_avg[j])
+    #print(monthly_avg[j])
     
   }
   else if (month[i] == month[i-1]){
     monthly_avg[j] = monthly_avg[j] + numeric_data[i]
-    print(monthly_avg[j])
+    #print(monthly_avg[j])
     month_iter = month_iter + 1
+    if (i==length(month)){
+      monthly_avg[j] = monthly_avg[j]/month_iter
+      month_year[j] <- paste0("", month[i], "/", year[i], "")
+    }
   }
   else if (month[i] != month[i-1]) {
     monthly_avg[j] = monthly_avg[j]/month_iter
@@ -75,10 +80,15 @@ for (i in 1:length(month)){
     print(j)
     monthly_avg[j] = numeric_data[i]
   }
+  
 }
+
+
 
 print(monthly_avg)
 print(month_year)
 
 my_dataframe <- data.frame(Time = month_year, Value = monthly_avg)
 print(my_dataframe)
+
+t
