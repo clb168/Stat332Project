13f11ae21916d1bcc55760c8515b09320262be47
Zach Brown <44880488+zbrown23@users.noreply.github.com>
2023-12-11 13:58:09 -0500

    general style cleanup

--- Project.R
+++ Project.R
@@ -1,91 +1,67 @@
-
 library(plotly)
 library(ggplot2)
 library(TTR)
 library(lubridate)
 library(tseries)
 library(forecast)
-# Read CSV file
-#my_data <- read.csv("/Users/cburh/Documents/Assignments_Fall2023/Stat_332/Project/SBUX.csv")
 
-#my_data[,1] <- as.Date(my_data[,1], format="%m/%d/%Y")
+# montly opening average
 
-#my_dataframe <- data.frame(Time = my_data[,1], Value = my_data[,4])
-
-#my_dataframe$Value <- as.numeric(gsub("[\\$,]", "", my_dataframe$Value))
-
-# time series plot 
-#ggplot(my_dataframe, aes(x = Time, y = Value)) +
-  #geom_line() +  # This adds a line plot
-  #xlab("\n Date") +
-  #ylab("Price in dollars \n")
-
-
-# decomposition
-#decomp_data = SMA(my_dataframe, n=30)
-#plot.ts(decomp_data)
-#print(decomp_data)
-
-#TESTING SEASONALIty
-#d = fft(my_dataframe)
-#decomposed_data=decompose(d,type='multiplicative')
-#autoplot(fft(decomposed_data))
-
-#partial auto correlation
-#acf(my_dataframe, type = c("partial"), plot = TRUE)
-
-#mean squared error
+# plot the time series
+# decomposition of time series
+# ACF and PCF
+# decide what kind of model is suitable for our data
+# test hypotheses to validate stationarity, estimate model parameters
 
 #montly averages
-my_data <- read.csv("/Users/cburh/Documents/Assignments_Fall2023/Stat_332/Project/SBUX.csv")
-Open_ts = ts(my_data[,1], frequency = 1)
+my_data <- read.csv("Stat332Project/SBUX.csv")
+Open_ts <- ts(my_data[, 1], frequency = 1)
 
-monthly_avg = array(0, dim = c(529))
-month=month(mdy(my_data[,1]))
+monthly_avg <- array(0, dim = c(529))
+month <- month(mdy(my_data[, 1]))
 # Extract the 4th column
 column_data <- my_data[, 4]
 
-week=week(mdy(my_data[,1]))
-month_year = array(" ", dim = c(529))
-year=year(mdy(my_data[,1]))
+week <- week(mdy(my_data[, 1]))
+month_year <- array(" ", dim = c(529))
+year <- year(mdy(my_data[,1]))
 #month_year[120] = "12/2013"
 # Remove the dollar sign and convert to numeric
 numeric_data <- as.numeric(sub("\\$", "", column_data))
 #print(month)
-temp = 5
-j=1
+temp <- 5
+j <- 1
 
-month_iter = 1
+month_iter <- 1
 for (i in 1:length(month)){
   #print(i)
   if (i==1){
-    monthly_avg[j] = numeric_data[1]
+    monthly_avg[j] <- numeric_data[1]
     #print(monthly_avg[j])
     
   }
   #changing this to weekly - to change back to monthly just use month[] instead of week[]
   else if (week[i] == week[i-1]){
-    monthly_avg[j] = monthly_avg[j] + numeric_data[i]
+    monthly_avg[j] <- monthly_avg[j] + numeric_data[i]
     #print(monthly_avg[j])
-    month_iter = month_iter + 1
+    month_iter <- month_iter + 1
     if (i==length(month)){
-      monthly_avg[j] = monthly_avg[j]/month_iter
+      monthly_avg[j] <- monthly_avg[j]/month_iter
       month_year[j] <- paste0("", week[i], "/", year[i], "")
     }
   }
   #changed to weekly - to change bach to monthly use month[]
   else if (week[i] != week[i-1]) {
-    monthly_avg[j] = monthly_avg[j]/month_iter
+    monthly_avg[j] <- monthly_avg[j]/month_iter
     month_year[j] <- paste0("", week[i-1], "/", year[i-1], "")
-    month_iter = 1
-    j = j+1
+    month_iter <- 1
+    j <- j+1
     print(j)
-    monthly_avg[j] = numeric_data[i]
+    monthly_avg[j] <- numeric_data[i]
   }
   
 }
-#HARDCODING this in 
-week[529] = '48/2013'
+week[529] <- '48/2013'
 
 
 
@@ -93,14 +69,14 @@ print(monthly_avg)
 print(month_year)
 
  
-month_yearRev = rev(month_year)
-month_avgRev = rev(monthly_avg)
+month_yearRev <- rev(month_year)
+month_avgRev <- rev(monthly_avg)
 my_dataframe <- data.frame(Time = month_yearRev, Value = month_avgRev)
 print(my_dataframe)
 #time series plus decomposition plots
-tsPlot = ts(my_dataframe[,2], frequency = 12)
+tsPlot <- ts(my_dataframe[,2], frequency = 12)
 plot(tsPlot)
-decomp=decompose(tsPlot, type=c("multiplicative"))
+decomp <- decompose(tsPlot, type=c("multiplicative"))
 plot(decomp)
 
 #training_set <- my_dataframe[1:100, ]  # First 100 rows for training
@@ -125,18 +101,18 @@ Box.test(tsPlot,lag=5,type = "Ljung-Box")
 Box.test(tsPlot,lag=5,type = "Box-Pierce")
 
 #fft stuff and splitting dataset for periodigram
-SBX_seasonal = as.vector(decomp$seasonal)
+SBX_seasonal <- as.vector(decomp$seasonal)
 #print(SBX_seasonal)
 
-train = SBX_seasonal[1:370]
-test = SBX_seasonal[371:529]
+train <- SBX_seasonal[1:370]
+test <- SBX_seasonal[371:529]
 print(test)
 print(train)
 
-Training = fft(train)
+Training <- fft(train)
 require(LSTS)
 
-TrainPgram = LSTS::periodogram(Training)
+TrainPgram <- LSTS::periodogram(Training)
 TrainPgram$plot
 
 # which ARIMA plot should we use?
@@ -163,15 +139,5 @@ data_trend = as.vector(decomp$trend)
 
 #Training and Testing Sets
 ntraining_set <- my_dataframe[21:120, ] 
-ytraining_set = data_trend[]
+ytraining_set <- data_trend[]
 ntesting_set <- my_dataframe[1:20, ]
-
-
-
-
-
-
-
-
-
-
